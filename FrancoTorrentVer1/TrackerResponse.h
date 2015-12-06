1448eba173fdba9651c6c99b0a1889abce06d4da
Yoav Franco <yfranco1999@gmail.com>
2015-12-06 22:56:35 +0200

    Persor bug fixed

--- FrancoTorrentVer1/TrackerResponse.h
+++ FrancoTorrentVer1/TrackerResponse.h
@@ -1,11 +1,11 @@
-﻿
-#pragma once
+﻿#pragma once
 
 #include "OrderedMap.h"
 #include "Bencoding.h"
 #include <sstream>
 #include <winsock.h>
 #include "TcpPeers.h"
+#include <string>
 
 class TrackerResponse
 {
@@ -35,21 +35,37 @@ public:
 	OrderedMap<std::string, unsigned short> DecodePeers5(char * buffer)
 	{
 		OrderedMap<std::string, unsigned short> peers;
+		/*std::string news(buffer, 36);
+
+		std::string ss = news.substr(news.size() - 2);
+
+		const char * bufferr = ss.c_str();
+
+		unsigned short pport3 = (bufferr[0] << 8) | bufferr[1]; 
+
+		//unsigned short port2 = (unsigned short) *bufferr;
+		int por = ntohs(pport3);  */
 		char *buf = buffer;
+
+
+
 		int chunks = strlen(buf) / 6;
 		int offset = 0;
 		unsigned char *ip;
-		unsigned short *port;
+		unsigned short port;
 		int recsDone = 0;
 
 	
 
 
 		while (recsDone < chunks){
-			ip = (unsigned char *)buf + offset;
-			port = (unsigned short *)buf + offset + 4;
-			peers.Insert(inet_ntoa(*(in_addr*)ip), ntohs(*port));
-			printf("\n\n%s - %d\n", inet_ntoa(*(in_addr*)ip), ntohs(*port));
+			ip = (unsigned char *) buf + offset;
+
+			port = (unsigned short) (buf[4] << 8) | buf[5];
+
+			//port = (unsigned short) buf + offset + 4;
+			peers.Insert(inet_ntoa(*(in_addr*)ip), (port));
+			printf("\n\n%s - %d\n", inet_ntoa(*(in_addr*)ip), (port));
 			//offset += 6;
 			buf += 6;
 			recsDone++;
